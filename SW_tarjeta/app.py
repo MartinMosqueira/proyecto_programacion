2bbf3bcf74fcee12ac01034ec8902b1fea2b90b9
martin <yomartinmosqueira331@gmail.com>
2020-03-19 12:37:00 -0300

    base de datos añadida

--- SW_tarjeta/app.py
+++ SW_tarjeta/app.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
+db = SQLAlchemy(app)
+
+class Tarjeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(15))
+    numero = db.Column(db.Integer)
+    cods = db.Column(db.Integer)
+
 
 @app.route('/tarjeta')
 def tarjeta():
-    return render_template("index.html")
+    tarjetas = Tarjeta.query.all()
+    return render_template("index.html", tarjetas=tarjetas)
 
 @app.route('/crear', methods=['POST'])
 def crear():
-    pass
+    tarjeta = Tarjeta(tipo = request.form['tipo'], numero = request.form['numero'], cods = request.form['cods'] )
+    db.session.add(tarjeta)
+    db.session.commit()
+    return redirect(url_for('tarjeta'))
+
+@app.route('/eliminar/<id>')
+def eliminar(id):
+    tarjeta = Tarjeta.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('tarjeta'))
+
 
 
 if __name__ == '__main__':
